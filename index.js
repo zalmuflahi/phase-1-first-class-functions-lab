ed1c0eabbd27e34a119b90ad3b1bcc348f93f45b
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-13 19:33:23 -0400

    commit tree 81edc9a42491d0166227a3071fa5529a0080ad53

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers(returnFirstTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']){ 
+    let temp = [returnFirstTwoDrivers[0],returnFirstTwoDrivers[1]]
+    return temp
+}
+
+function returnLastTwoDrivers(returnLastTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']){
+    let size = returnLastTwoDrivers.length
+    let temp = [returnLastTwoDrivers[size-2],returnLastTwoDrivers[size-1]]
+    return temp
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(value){
+    return function (multiplier){
+        return value * multiplier;
+    } 
+
+}
+function fareDoubler(val){
+        return val * 2
+}
+function fareTripler(va){
+    return va * 3
+}
+function selectDifferentDrivers(array,function_name){
+    return function_name(array)
+}
